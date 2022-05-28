d2737becaf73ed249544018405244d9dc781d8cf
aliencdh <cdevhax@gmail.com>
2022-05-28 22:25:32 +0200

    switched to rust

--- src/main.c
+++
@@ -1,5 +0,0 @@
-
-extern void _start() {
-    *(char*)0xb8000 = 'Q';
-    return;
-}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+static MSG: &[u8] = b"Hello World!";
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in MSG.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f;
+        }
+    }
+
+    loop {}
+}
+
